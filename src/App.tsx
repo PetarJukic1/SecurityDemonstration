9d1533cc46afd62d1624689c06f84d83d865ff4d
PetarJukic1 <petar.jukic@flabbergast.agency>
2022-11-05 22:51:40 +0100

    XSS example implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {createRef} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const divRef = createRef<HTMLDivElement>();
+    const inputRef = createRef<HTMLInputElement>();
+    const data = "Nothing submitted yet."
+    const [checked, setChecked] = useState(false);
+
+    const handleChange = () => {
+        setChecked(!checked);
+    };
+
+    const onSubmit = () => {
+        if (inputRef.current && divRef.current) {
+            console.log(inputRef.current.value)
+            if (checked) {
+                divRef.current.innerText = inputRef.current.value
+            } else {
+                divRef.current.innerHTML = inputRef.current.value
+            }
+        }
+    }
+
+    return (
+        <div className="App">
+            <div className="container">
+                <h1> XSS threat </h1>
+            </div>
+            <input ref={inputRef}/>
+            <button className="button" onClick={() => onSubmit()}>Submit</button>
+            Disable XSS:
+            <input type="checkbox" onChange={handleChange} checked={checked}/>
+            <body>Input: </body>
+
+            <div ref={divRef}>
+                {data}
+            </div>
+            <h3>Example:</h3>
+            <div>
+                To emulate xss try to input {"<img src onError=\"alert(document.cookie)\">"}
+            </div>
+        </div>
+    );
 }
 
 export default App;
